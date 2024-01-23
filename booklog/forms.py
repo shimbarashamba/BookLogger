e148d06f03df4772a2d24778ad26552c09c5c2b6
Axel Abrahamsson <73606183+shimbarashamba@users.noreply.github.com>
2024-01-23 12:15:22 +0100

    Update forms.py

--- booklog/forms.py
+++ booklog/forms.py
@@ -6,4 +6,4 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author']  # Replace 'other_fields' with actual fields from your model
+        fields = ['title', 'author']
